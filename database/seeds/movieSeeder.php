ab5a29cca4a51fa5e971ceb595a5f6e90f57f2ff
Mario <lombardomario01@gmail.com>
2022-01-31 15:24:03 +0100

    Seeder added

--- database/seeds/movieSeeder.php
+++ database/seeds/movieSeeder.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Movie;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class movieSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(movieSeeder::class);
+        factory(Movie::class, 100)->create();
     }
 }
